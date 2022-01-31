bfe302a0ad3ce3047b186b42a7ea7c41f76e8bbd
ken <no-reply@princeton.edu>
2022-01-31 01:37:06 -0800

    laptop2

--- paper/arxiv_filter.py
+++ paper/arxiv_filter.py
@@ -20,13 +20,13 @@ with open(json_load_path, "r") as f:
 		print(a["id"])
 		year = a["id"][0:4]
 		if year >= start:	
-			flag = 0
+			flag = [False, False]
 			for item in a["categories"].split(" "):
 				# if item.startswith("q-fin") and "arbitrage" in a["title"].lower():
 				if item.startswith("q-fin"):
-					flag += 1
+					flag[0] = True
 				elif item == "cs.CL":
-					flag += 1
+					flag[1] = True
 				
 				if flag == 2:
 					csv_data.append([a["id"], a["title"].replace("\n", ""), a["authors_parsed"][0][1] + " " + a["authors_parsed"][0][0], a["categories"]])
