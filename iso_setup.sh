852d6b5c5f79ce419314eda4c51536eebd857e40
ken <no-reply@princeton.edu>
2022-02-06 00:56:57 -0800

    laptop2

--- iso_setup.sh
+++ iso_setup.sh
@@ -6,6 +6,7 @@
 
 
 pacman -Syy
+pacman -S archlinux-keyring
 
 lsblk
 printf "Enter device name, boot_idx, system_idx (both including suffix), g if want a new partition table"; read dname bid sid erase
