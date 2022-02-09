1d3f1580c73d94d3cee22f0ca886c41487ba3aaa
ken <no-reply@princeton.edu>
2022-02-08 23:49:46 -0800

    laptop1

--- init.sh
+++ init.sh
@@ -31,7 +31,12 @@ initFunc() {
 			pacman -Syyu --noconfirm
 			
 			cp /boot/vmlinuz-linux /efi/
+			# use kernel for console font, equivalent.
+			sed -i '/^HOOKS=/ s/.*/HOOKS=(base udev autodetect keyboard modconf block encrypt filesystems fsck)/g' /etc/mkinitcpio.conf
+			mkinitcpio -g /efi/initramfs-linux.img -c /etc/mkinitcpio.conf -k /efi/vmlinuz-linux
 			
+			umount /efi
+			rmdir /efi
 		fi
 		cur_date=$temp_date
 		sleep 43200
