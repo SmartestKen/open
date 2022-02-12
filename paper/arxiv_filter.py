ddddf9cfad25110ec69a733b14a908365913baf6
ken <no-reply@princeton.edu>
2022-02-12 01:37:04 -0800

    laptop2

--- paper/arxiv_filter.py
+++ paper/arxiv_filter.py
@@ -18,10 +18,7 @@ with open(json_load_path, "r") as f:
 	for line in f:
 		a = json.loads(line)	
 		print(a["id"])
-		if a["id"] == "1910.14424":
-			print(a)
-			print("ranking" in a["title"].lower(), "ranking" in a["abstract"].lower())
-			time.sleep(200000)
+
 		# print(a)
 		# time.sleep(200000)
 		year = a["id"][0:4]
@@ -29,7 +26,7 @@ with open(json_load_path, "r") as f:
 			# flag = [False, False]
 			flag = [False]
 			for item in a["categories"].split(" "):
-				if item.startswith("cs.CL") and ("ranking" in a["title"].lower() or "ranking" in a["abstract"].lower()):
+				if (item.startswith("cs.CL") or item.startswith("cs.IR")) and ("ranking" in a["title"].lower() or "ranking" in a["abstract"].lower()):
 					flag[0] = True
 				'''
 				if item.startswith("q-fin"):
