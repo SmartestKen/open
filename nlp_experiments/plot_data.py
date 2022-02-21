5008e92b07cc6d647f71931a209baf58ac06da52
ken <no-reply@princeton.edu>
2022-02-20 20:03:06 -0800

    laptop2

--- nlp_experiments/plot_data.py
+++ nlp_experiments/plot_data.py
@@ -4,6 +4,11 @@ sys.path.append("/home/ken/private")
 from api_access import *
 session = aiohttp.ClientSession()
 
+async def generic_get(url):
+	# get the url and return html
+	# if cannot get sys.exit
+
+
 
 
 
@@ -24,6 +29,9 @@ async def EOD_get_data(start, symbol, tempfile = "/tmp/data.html"):
 loop = asyncio.get_event_loop()
 bars = loop.run_until_complete(loop.create_task(EOD_get_data("GSPC.INDX", "1y")))
 
+
+
+https://eodhistoricaldata.com/api/exchanges-list/?api_token=YOUR_API_TOKEN&fmt=json
 1y 1m 1w today
 print(bars)
 loop.run_until_complete(loop.create_task(session.close()))
