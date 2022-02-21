fa1356e213307e337a4d0362eb385f7540050690
ken <no-reply@princeton.edu>
2022-02-20 23:38:57 -0800

    laptop2

--- nlp_experiments/plot_data.py
+++ nlp_experiments/plot_data.py
@@ -14,15 +14,19 @@ async def generic_get(url):
 			await f.write(html)
 				
 		if response.status == 200:
-			return json.loads(html)
+			return html
 		else:
 			sys.exit(html)
 
 
 
 async def EOD_get_data(start, symbol, tempfile = "/tmp/data.html"):
+	url = "https://eodhistoricaldata.com/api/eod/" + symbol + suffix + "?api_token=" + EOD_KEY + "&fmt=json&from=" + http_start + "&to=" + end
+	html = await generic_get(url)
+	return json.load(html)
 
-		
+
+
 		
 loop = asyncio.get_event_loop()
 bars = loop.run_until_complete(loop.create_task(EOD_get_data("GSPC.INDX", "1y")))
