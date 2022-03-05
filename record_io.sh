d9a833f0613cf8c4bf856b8e0ee2dad1e3717553
ken <no-reply@princeton.edu>
2022-03-05 13:55:34 -0800

    laptop2

--- record_io.sh
+++ record_io.sh
@@ -1,4 +1,5 @@
 pacmd load-module module-null-sink sink_name=dummy sink_properties=device.description=recording
+pactl list sources
 pactl load-module module-loopback sink=dummy
 pactl load-module module-loopback sink=dummy
 
