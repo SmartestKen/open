9c97737331bf0ff449e795583d5ab84338e71143
ken <no-reply@princeton.edu>
2022-03-05 15:22:45 -0800

    laptop2

--- record_io.sh
+++ record_io.sh
@@ -1,21 +1,10 @@
 # list all sources name for loopback
 sources=`pactl list sources | grep Name: | sed 's/^.*Name: //'`
 if [[ $sources != *"dummy"* ]]
-then
-	
+then	
+	pactl load-module module-null-sink sink_name=dummy sink_properties=device.description=Recording
+	while IFS= read -r item
+	do
+		pactl load-module module-loopback source=$item sink=dummy
+	done <<<"$sources"
 fi
-
-
-# null-sink
-pactl load-module module-null-sink sink_name=dummy sink_properties=device.description=Recording
-
-pactl load-module module-loopback source=$temp1 sink=dummy
-pactl load-module module-loopback source=$temp2 sink=dummy
-
-pactl load-module module-loopback source=VoIP.monitor sink=alsa_output.pci-0000_00_1b.0.analog-stereo latency_msec=200
-pactl load-module module-loopback source=Games.monitor sink=alsa_output.pci-0000_00_1b.0.analog-stereo latency_msec=50
-
-
-# pacmd load-module module-combine-sink sink_name=Combined slaves=dummy,alsa_output.usb-Sennheiser_Communications_Sennheiser_USB_headset-00.analog-stereo sink_properties=device.description=Combined
-
-pacmd load-module module-loopback source=$temp2 sink=dummy latency_msec=1
