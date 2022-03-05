f9ad3bac808e4ef8c5a3dc58eaa4394b527c51e6
ken <no-reply@princeton.edu>
2022-03-05 13:45:18 -0800

    laptop2

--- record_io.sh
+++ record_io.sh
@@ -1,4 +1,4 @@
-pacmd load-module module-null-sink sink_name=dummy sink_properties=device.description=Virtual-Sound-Card
+pacmd load-module module-null-sink sink_name=dummy sink_properties=device.description=recording
 pactl load-module module-loopback sink=dummy
 pactl load-module module-loopback sink=dummy
 
@@ -6,8 +6,6 @@ pactl load-module module-loopback source=VoIP.monitor sink=alsa_output.pci-0000_
 pactl load-module module-loopback source=Games.monitor sink=alsa_output.pci-0000_00_1b.0.analog-stereo latency_msec=50
 
 
-
-pacmd load-module module-null-sink sink_name=dummy sink_properties=device.description=Recording
 pacmd load-module module-combine-sink sink_name=Combined slaves=dummy,alsa_output.usb-Sennheiser_Communications_Sennheiser_USB_headset-00.analog-stereo sink_properties=device.description=Combined
 
 pacmd load-module module-loopback source=$temp2 sink=dummy latency_msec=1
