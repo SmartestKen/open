e50ef8bac6b3d8ca2098248cd377a95e11a3650a
ken <no-reply@princeton.edu>
2022-03-05 14:00:41 -0800

    laptop2

--- record_io.sh
+++ record_io.sh
@@ -1,12 +1,13 @@
 pacmd load-module module-null-sink sink_name=dummy sink_properties=device.description=recording
 pactl list sources
-pactl load-module module-loopback sink=dummy
-pactl load-module module-loopback sink=dummy
+printf "use pauvcontrol, decide the sources"; read temp1 temp2
+pactl load-module module-loopback source=$temp1 sink=dummy
+pactl load-module module-loopback source=$temp2 sink=dummy
 
 pactl load-module module-loopback source=VoIP.monitor sink=alsa_output.pci-0000_00_1b.0.analog-stereo latency_msec=200
 pactl load-module module-loopback source=Games.monitor sink=alsa_output.pci-0000_00_1b.0.analog-stereo latency_msec=50
 
 
-pacmd load-module module-combine-sink sink_name=Combined slaves=dummy,alsa_output.usb-Sennheiser_Communications_Sennheiser_USB_headset-00.analog-stereo sink_properties=device.description=Combined
+# pacmd load-module module-combine-sink sink_name=Combined slaves=dummy,alsa_output.usb-Sennheiser_Communications_Sennheiser_USB_headset-00.analog-stereo sink_properties=device.description=Combined
 
 pacmd load-module module-loopback source=$temp2 sink=dummy latency_msec=1
