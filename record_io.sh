a39d74b33e50b13289698de27b446e0776e73ea8
ken <no-reply@princeton.edu>
2022-03-05 13:09:26 -0800

    laptop2

--- record_io.sh
+++ record_io.sh
@@ -2,7 +2,12 @@ pacmd load-module module-null-sink sink_name=dummy sink_properties=device.descri
 pactl load-module module-loopback sink=dummy
 pactl load-module module-loopback sink=dummy
 
+pactl load-module module-loopback source=VoIP.monitor sink=alsa_output.pci-0000_00_1b.0.analog-stereo latency_msec=200
+pactl load-module module-loopback source=Games.monitor sink=alsa_output.pci-0000_00_1b.0.analog-stereo latency_msec=50
 
-pacmd load-module module-null-sink sink_name=Recording sink_properties=device.description=Recording
-pacmd load-module module-combine-sink sink_name=Combined slaves=Recording,alsa_output.usb-Sennheiser_Communications_Sennheiser_USB_headset-00.analog-stereo sink_properties=device.description=Combined
-pacmd load-module module-loopback source=$temp2 sink=Recording latency_msec=1
+
+
+pacmd load-module module-null-sink sink_name=dummy sink_properties=device.description=Recording
+pacmd load-module module-combine-sink sink_name=Combined slaves=dummy,alsa_output.usb-Sennheiser_Communications_Sennheiser_USB_headset-00.analog-stereo sink_properties=device.description=Combined
+
+pacmd load-module module-loopback source=$temp2 sink=dummy latency_msec=1
