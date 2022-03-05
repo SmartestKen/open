ffb6468b9de01f15171bec7b8123962dc317dae8
ken <no-reply@princeton.edu>
2022-03-05 15:27:52 -0800

    laptop2

--- record.sh
+++ record.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 
+# list all sources name for loopback
+sources=`pactl list sources | grep Name: | sed 's/^.*Name: //'`
+if [[ $sources != *"dummy"* ]]
+then	
+	# to remove, use `pactl unload-module module-null-sink`
+	pactl load-module module-null-sink sink_name=dummy sink_properties=device.description=Recording
+	while IFS= read -r item
+	do
+		pactl load-module module-loopback source=$item sink=dummy
+	done <<<"$sources"
+fi
+
 
 if xrandr | grep 'HDMI1 connected' >/dev/null
 then
--- record_io.sh
+++ record_io.sh
@@ -2,6 +2,7 @@
 sources=`pactl list sources | grep Name: | sed 's/^.*Name: //'`
 if [[ $sources != *"dummy"* ]]
 then	
+	# to remove, use `pactl unload-module module-null-sink`
 	pactl load-module module-null-sink sink_name=dummy sink_properties=device.description=Recording
 	while IFS= read -r item
 	do
