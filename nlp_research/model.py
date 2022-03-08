cf7e2acaf1d1f71bedb1fd7a3d68cbaded021fc2
ken <no-reply@princeton.edu>
2022-03-08 01:02:39 -0800

    laptop2

--- nlp_research/model.py
+++ nlp_research/model.py
@@ -3,14 +3,14 @@ import tensorflow as tf
 class BertConfig():
 	def __init__(self):
 		self.vocab_size = 
-		self.hidden_size = 768,
-		self.num_hidden_layers=12,
-		self.num_attention_heads=12,
-		self.intermediate_size=3072,
-		self.hidden_act="gelu",
-		self.hidden_dropout_prob=0.1,
-		self.attention_probs_dropout_prob=0.1,
-		self.max_position_embeddings=512,
+		self.hidden_size = 768
+		self.num_hidden_layers = 12
+		self.num_attention_heads = 12
+		self.intermediate_size = 3072
+		self.hidden_act = "gelu"
+		self.hidden_dropout_prob = 0.1
+		self.attention_probs_dropout_prob = 0.1
+		self.max_position_embeddings = 512
 		self.type_vocab_size = 16
 		self.initializer_range = 0.02
 
