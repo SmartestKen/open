7931fa406a28fc4f4a380dbc31a7e179a3043022
ken <no-reply@princeton.edu>
2022-03-08 16:58:05 -0800

    laptop2

--- nlp_research/preprocessing.py
+++ nlp_research/preprocessing.py
@@ -1,4 +1,12 @@
 import model
 
+# cross entropy and regression loss?
+# run_classifier.py
+# what is input form, objective
+
+
+
+
+
 # both pre-train and fine-tune adjust weights, it is just that pretrain usually involve large corpus
 config = model.BertConfig()
