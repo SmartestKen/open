ed4d11ee1ea33a7bec87ff6a9c337f86bb429f05
ken <no-reply@princeton.edu>
2022-03-08 16:01:35 -0800

    laptop2

--- nlp_research/preprocessing.py
+++ nlp_research/preprocessing.py
@@ -1,12 +1,3 @@
-config = dict()
-config["vocab_size"] = vocab_size
-config["hidden_size"] = hidden_size
-config["num_hidden_layers"] = num_hidden_layers
-config["num_attention_heads"] = num_attention_heads
-self.hidden_act = hidden_act
-self.intermediate_size = intermediate_size
-self.hidden_dropout_prob = hidden_dropout_prob
-self.attention_probs_dropout_prob = attention_probs_dropout_prob
-self.max_position_embeddings = max_position_embeddings
-self.type_vocab_size = type_vocab_size
-self.initializer_range = initializer_range
+import model
+
+config = model.BertConfig()
