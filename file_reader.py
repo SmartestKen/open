ca9544e17d2c67f0ba59f564ca5d15bf43f3dda6
ken <no-reply@princeton.edu>
2022-03-09 16:42:23 -0800

    laptop2

--- file_reader.py
+++ file_reader.py
@@ -1,8 +1,10 @@
 import sys
-print(sys.argv[1])
 
-'''
-with open("datafile") as myfile:
+if len(sys.argv) == 1:
+	sys.exit("Require filename as argument")
+	
+# print first line, each line first few characters and last few
+with open(sys.argv[1]) as myfile:
     head = [next(myfile) for x in range(200)]
 print(head)
-'''
+
