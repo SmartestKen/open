03a1d9507b4d8c31555eb11a7b503ecc187f50b5
ken <no-reply@princeton.edu>
2022-03-11 02:23:45 -0800

    laptop2

--- nlp_research/preprocessing.py
+++ nlp_research/preprocessing.py
@@ -7,6 +7,18 @@ import model
 
 # music/anime/novel, ranking/sort, not necessarily nn methods
 # either minimal supervised or popularity (e.g. reddit news, tuishujun)
+# coding experiment (what is the target ouput of marco?), send an email to 
+
+'''
+Four of the most important theory/idea in Financial math that you will need a good grasp on: Ito's Lemma (pretty much like construction of differentiation in normal calculus), Girsanov theorem, Feymann-Kac (which is just Kolmogorov equations tbh) and martingale representation theorem.
+
+P74 4.6.1
+'''
+
+
+# residual --> hence hidden size = word_size
+
+
 
 
 
