ca4f073205a594d0843400337e5431b9f9fd871e
ken <no-reply@princeton.edu>
2022-03-11 04:27:13 -0800

    laptop2

--- nlp_research/plot_data.py
+++ nlp_research/plot_data.py
@@ -21,7 +21,13 @@ async def generic_get(url, tempfile = "/tmp/data.html"):
 		else:
 			sys.exit(html)
 
+# expect format 
 async def EOD_get_intraday_data(start, symbol_list):
+	
+	date_time = '29.08.2011 11:05:02'
+pattern = '%d.%m.%Y %H:%M:%S'
+	start = int(time.mktime(time.strptime(start, pattern)))
+	
 	bars_list = []
 	for symbol in symbol_list:
 		url = "https://eodhistoricaldata.com/api/intraday/" + symbol + "?api_token=" + EOD_KEY + "&fmt=json&interval=30m&from=" + start
@@ -34,4 +40,4 @@ async def EOD_get_intraday_data(start, symbol_list):
 	
 	
 symbol_list = ["GSPCC.INDX"]
-
+loop.run_until_complete(loop.create_task(EOD_get_intraday_data("2010-01-01
