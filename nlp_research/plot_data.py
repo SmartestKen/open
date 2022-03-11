6ea34a736d0e28d7a083e5064603d029badbe90c
ken <no-reply@princeton.edu>
2022-03-11 04:52:57 -0800

    laptop2

--- nlp_research/plot_data.py
+++ nlp_research/plot_data.py
@@ -25,24 +25,21 @@ async def generic_get(url, tempfile = "/tmp/data.html"):
 
 # expect start format '%Y-%m-%d %H:%M:%S'
 # start-end max 3600 days
-async def EOD_get_intraday_data(start, end, interval, symbol):
+async def EOD_get_intraday_data(start, end, symbol, interval = "1h"):
 	# convert to epoch
 	start = str(int(time.mktime(time.strptime(start, '%Y-%m-%d %H:%M:%S'))))
 	end = str(int(time.mktime(time.strptime(end, '%Y-%m-%d %H:%M:%S'))))
 
 	bars_list = []
-	for symbol in symbol_list:
-		url = "https://eodhistoricaldata.com/api/intraday/" + symbol + "?api_token=" + EOD_KEY + "&fmt=json&interval=30m&from=" + start + "&to=" + end
-		html = await generic_get(url)
-		# print(html)
-		bars_list.append(json.loads(html))
-		
-	# print(bars_list)
-	return bars_list
+
+	url = "https://eodhistoricaldata.com/api/intraday/" + symbol + "?api_token=" + EOD_KEY + "&fmt=json&interval=30m&from=" + start + "&to=" + end + "&interval=" + interval
+	html = await generic_get(url)
+
+	return json.loads(html)
 	
 	
-symbol_list = ["GSPC.INDX"]
-bars_list = loop.run_until_complete(loop.create_task(EOD_get_intraday_data("2010-01-01 00:00:00", "2016-01-01 00:00:00", symbol_list)))
+symbol = "GSPC.INDX"
+bars_list = loop.run_until_complete(loop.create_task(EOD_get_intraday_data("2010-01-01 00:00:00", "2015-03-01 00:00:00", symbol)))
 # bars_list = loop.run_until_complete(loop.create_task(EOD_get_intraday_data("2018-01-01 00:00:00", "2022-01-01 00:00:00", symbol_list)))
 print(bars_list)
 loop.run_until_complete(loop.create_task(session.close()))
