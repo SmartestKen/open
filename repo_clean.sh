f6b44795b834db26e7dc60e2bbbff70540c6c18e
ken <no-reply@princeton.edu>
2022-03-15 05:41:07 -0700

    laptop0

--- repo_clean.sh
+++ repo_clean.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-clean_repo() {
-
-cd $1
-mv .git/config /tmp/config
-mv .git/info/attributes /tmp/attributes
-rm -rf .git
-git init
-mv /tmp/config .git/config
-mv /tmp/attributes .git/info/attributes 
-
-
-ignore_files=`find . -type f -size +75M -not -path '*/\.git/*'`
-if [[ $ignore_files != "" ]]
-then
-	sed 's|^\./||g' >"$1"/.git/info/exclude <<< "$ignore_files"
-	echo "$ignore_files" | xargs git rm --cached 2>/dev/null
-fi
-git add .
-git commit -m "$2"
-git push -f origin master
-
-}
-
 pkill sync.sh
-index=`tail -c 2 /etc/hostname`
-# clean_repo /home/ken/open laptop$index
-clean_repo /home/ken/private laptop$index
-# clean_repo /home/ken/clips laptop$index
+repo_locations="/home/ken/open
+/home/ken/private
+/home/ken/clips"
+device=`cat /etc/hostname`
+
+while IFS= read -r repo
+do
+	cd $repo
+	mv .git/config /tmp/config
+	mv .git/info/attributes /tmp/attributes
+	rm -rf .git
+	git init
+	mv /tmp/config .git/config
+	mv /tmp/attributes .git/info/attributes 
+
+	# ignore_files=`find . -type f -size +75M -not -path '*/\.git/*'`
+	# if [[ $ignore_files != "" ]]
+	# then
+		# sed 's|^\./||g' >"$1"/.git/info/exclude <<< "$ignore_files"
+		# echo "$ignore_files" | xargs git rm --cached 2>/dev/null
+	# fi
+	git add .
+	git commit -m "$device"
+	git push -f origin master
+
+done <<<"$repo_locations"
