6d599a5df37243ef7edbbfa02b23c249d5811fe6
ken <no-reply@princeton.edu>
2022-03-14 23:15:00 -0700

    laptop0

--- root_setup_old.sh
+++ root_setup_old.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# now continue setup inside the chroot
+
 
 ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 date -s "$(curl --head http://google.com 2>&1 | grep Date: | cut -d' ' -f3-6)Z"
